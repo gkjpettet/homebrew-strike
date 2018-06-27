1e134bf72cc0dfd9bd3e8c76c4d9293cc021f3a1
Garry Pettet <contact@garrypettet.com>
2018-06-27 14:44:24 +0100

    Version 0.9.3

--- Formula/strike3.rb
+++ Formula/strike3.rb
@@ -1,8 +1,8 @@
 class Strike3 < Formula
   desc "Strike3- A Static Site Generator"
   homepage "https://github.com/gkjpettet/strike3"
-  url "https://github.com/gkjpettet/strike3-cli/files/2141063/strike3-092-macos.zip"
-  sha256 "02bd637a1d3c902706656d1df959e1879a01bfe480fff8c0c6a388d638e7bc3e" # openssl sha256 [file]
+  url "https://github.com/gkjpettet/strike3-cli/files/2141479/strike3-093-macos.zip"
+  sha256 "6e6acf53d6965451b14db649021353ea04778e438c9b5b809dc166eb2f5c0c2f" # openssl sha256 [file]
 
   def install
     bin.install "404.html"
