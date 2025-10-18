6996c9f75d63829552ac182a6325d838caa01ba6
Garry Pettet <contact@garrypettet.com>
2025-10-18 15:24:42 +0100

    Version 1.6.3

--- Formula/strike.rb
+++ Formula/strike.rb
@@ -1,11 +1,10 @@
 class Strike < Formula
   desc "Strike- A Static Site Generator"
   homepage "https://github.com/gkjpettet/strike"
-  url "https://github.com/gkjpettet/strike/releases/download/1.6.2/strike-162-macos.zip"
-  sha256 "1547b2670f475982fe2eb4c45628540b8e802776f83b3c2adb4e09b0a12ac0a9" # openssl sha256 [file]
+  url "https://github.com/gkjpettet/strike/releases/download/1.6.3/strike-163-macos.zip"
+  sha256 "7843e98adcbd90accd8b9ed8b98c26d01ba2be883abbdb5f21319b201672c917" # openssl sha256 [file]
 
   def install
-    bin.install "_CodeSignature"
     bin.install "strike Libs"
     bin.install "strike"
   end
